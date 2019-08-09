4c2dc0ac60848c672692c50b642a29d00c85c7f3
William Miner <wgminer@gmail.com>
2019-08-08 22:58:33 -0400

    Update design

--- js/main.js
+++ js/main.js
@@ -14,14 +14,23 @@ $(function () {
     	audioElement.play();
 	}
 
+	audioElement.addEventListener('timeupdate', function() {
+		var currentTime = audioElement.currentTime;
+		var duration = audioElement.duration;
+		$('.active .bar').css('width', currentTime/duration * 100 + '%');
+	});
+
     $('.item').mousedown(function () {
-    	let { path } = $(this).data();
+		let { path } = $(this).data();
+		$(this).addClass('active');
     	audioElement.setAttribute('src', path);
     	audioElement.addEventListener('canplay', handleCanPlay);
     }).mouseup(function () {
     	audioElement.pause();
     	audioElement.removeEventListener('canplay', handleCanPlay)
-    	audioElement.currentTime = 0;
+		audioElement.currentTime = 0;
+		$(this).removeClass('active');
+		$(this).find('.bar').css('width', 0);
     });
     
 });
