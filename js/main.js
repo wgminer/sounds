f501dfc480b16e936f5e9faa8cf27e3d5b0b2d4f
Will Miner <wgminer@gmail.com>
2021-05-31 20:17:35 -0500

    Quick save

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
 $(function () {
+  console.log("HELLO");
 
-	console.log('HELLO')
+  let audioElement = document.createElement("audio");
 
-	let audioElement = document.createElement('audio');   
+  audioElement.volume = 1;
 
-	audioElement.volume = 1; 
+  audioElement.addEventListener(
+    "ended",
+    function () {
+      this.play();
+    },
+    false
+  );
 
-    audioElement.addEventListener('ended', function() {
-        this.play();
-    }, false);
- 
-    function handleCanPlay(){
-    	audioElement.play();
-	}
+  function handleCanPlay() {
+    audioElement.play();
+  }
 
-	audioElement.addEventListener('timeupdate', function() {
-		var currentTime = audioElement.currentTime;
-		var duration = audioElement.duration;
-		$('.active .bar').css('width', currentTime/duration * 100 + '%');
-	});
+  audioElement.addEventListener("timeupdate", function () {
+    var currentTime = audioElement.currentTime;
+    var duration = audioElement.duration;
+    $(".active .bar").css("width", (currentTime / duration) * 100 + "%");
+  });
 
-    $('.item').mousedown(function () {
-		let { path } = $(this).data();
-		$(this).addClass('active');
-    	audioElement.setAttribute('src', path);
-    	audioElement.addEventListener('canplay', handleCanPlay);
-    }).mouseup(function () {
-    	audioElement.pause();
-    	audioElement.removeEventListener('canplay', handleCanPlay)
-		audioElement.currentTime = 0;
-		$(this).removeClass('active');
-		$(this).find('.bar').css('width', 0);
+  $(".cell")
+    .mousedown(function () {
+      let { path } = $(this).data();
+      $(this).addClass("active");
+      audioElement.setAttribute("src", path);
+      audioElement.addEventListener("canplay", handleCanPlay);
+    })
+    .mouseup(function () {
+      audioElement.pause();
+      audioElement.removeEventListener("canplay", handleCanPlay);
+      audioElement.currentTime = 0;
+      $(this).removeClass("active");
+      $(this).find(".bar").css("width", 0);
     });
-    
 });
