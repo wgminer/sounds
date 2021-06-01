8ec7d653541186659a3c6a6e40153acb58e19480
Will Miner <wgminer@gmail.com>
2021-05-31 21:13:01 -0500

    Update build.js

--- build.js
+++ build.js
@@ -1,40 +1,44 @@
-const fs = require('fs');
-const pug = require('pug');
-const changeCase = require('change-case');
+const fs = require("fs-extra");
+const pug = require("pug");
+const changeCase = require("change-case");
 
-// Read manifest
-let manifest = require('./manifest.json');
+const rawPath = __dirname + "/raw/";
+const audioPath = __dirname + "/audio/";
+
+fs.removeSync(audioPath); // I just deleted my entire HOME directory.
 
 // Read files
-let files = fs.readdirSync('./audio').filter(f => f != '.DS_Store');
+let files = fs.readdirSync("./raw").filter((f) => f != ".DS_Store");
+let move = files.map((fileName) => {
+  return {
+    newPath: audioPath + fileName.split(" ").join("_").toLowerCase(),
+    oldPath: rawPath + fileName,
+    fileName: fileName.split(" ").join("_").toLowerCase(),
+  };
+});
 
-for (var i = 0; i < files.length; i++) {
-	let f = files[i];
-	fs.renameSync('./audio/' + f, './audio/' + f.split(' ').join('_').toLowerCase());
+let manifest = [];
+
+move.forEach((file) => {
+  fs.copySync(file.oldPath, file.newPath);
+
+  manifest.push({
+    title: changeCase.titleCase(file.fileName.split(".")[0]).trim(),
+    path: "/audio/" + file.fileName,
+  });
+});
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
-files = files.map(f => f.split(' ').join('_').toLowerCase());
-
-files.forEach(f => {
-
-	// Add new files
-	if (manifest.findIndex(m => m.fileName == f) == -1) {
-		manifest.push({
-			date: '',
-			title: changeCase.title(f.split('.')[0]),
-			path: 'audio/' + f,
-			fileName: f,
-		});
-	}
-
+// Write HTML
+let html = pug.renderFile(`./pug/index.pug`, {
+  pretty: true,
+  files: manifest.sort((a, b) => (a.title > b.title ? 1 : -1)),
 });
 
-// Rewrite manifest
-
-let html =  pug.renderFile(`./pug/index.pug`, { 
-	pretty: true,
-	files: manifest 
-});
-
-fs.writeFileSync('manifest.json', JSON.stringify(manifest, null, 4));
-fs.writeFileSync('index.html', html);
+fs.writeFileSync("index.html", html);
